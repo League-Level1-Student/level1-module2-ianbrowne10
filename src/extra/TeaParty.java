8c82d27559a7dc30bdee778c93aff41e9aa36277
League <League@AdminisorsiMac2.attlocal.net>
2019-01-05 11:01:39 -0800

    TeaParty

--- src/extra/TeaParty.java
+++ src/extra/TeaParty.java
@@ -2,4 +2,16 @@ package extra;
 
 public class TeaParty {
 
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		if (isWoman == true && isKnighted == true) {
+			return ("Hello Lady " + name);
+		} else if (isWoman == true && isKnighted == false) {
+			return ("Hello Ms. " + name);
+		} else if (isWoman == false && isKnighted == true) {
+			return ("Hello Sir " + name);
+		} else {
+			return ("Hello Mr. " + name);
+		}
+	}
+
 }
